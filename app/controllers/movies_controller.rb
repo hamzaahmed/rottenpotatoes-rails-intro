87223d4fb1c1b0394c2e557f05825db5cf7a056c
hamzaahmed <hamza.ah@berkeley.edu>
2016-02-15 23:43:40 +0000

    Finished assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,53 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    temp_ratings = params[:ratings]
+    fresh = {}
+
+    if temp_ratings.nil?
+      #No ratings from current event
+      if session[:selected_ratings].nil?
+        #No ratings stored in session
+        temp_ratings = @all_ratings
+      else
+        #Ratings are retrieved from the session
+        temp_ratings = session[:selected_ratings]
+        fresh[:ratings] = temp_ratings
+      end
+    else
+      #Some ratings were selected during current event
+      temp_ratings = temp_ratings.keys
+    end
+
+    @selected_ratings = temp_ratings
+    session[:selected_ratings] = temp_ratings
+    @title_class = ""
+    @date_class = ""
+    @movies = Movie.where({rating: [@selected_ratings]})
+    sort = nil
+    if params.has_key?(:sort)
+      sort = params[:sort].to_sym
+    elsif not session[:sort].nil?
+      #Sort based on what's stored in the session
+      sort = session[:sort].to_sym
+      fresh[:sort] = sort
+    end
+    if not sort.nil?
+        session[:sort] = sort
+      if sort == :title
+        @title_class = "hilite"
+        @movies = @movies.order(:title)
+      elsif sort == :release_date
+        @date_class = "hilite"
+        @movies = @movies.order(:release_date)
+      end
+    end
+
+    if not fresh == {}
+        flash.keep
+        redirect_to :sort => sort, :ratings => Hash[@selected_ratings.map {|rating| [rating, 1]}] and return
+    end
   end
 
   def new
